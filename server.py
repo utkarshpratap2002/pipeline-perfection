14490ab4c2f33913b31c2be0381e9cea523e831d
Utkarsh Pratap Singh <itsutkarshpratapon@gmail.com>
2024-12-29 21:34:28 +0530

    version:v2

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root():
-    return "flask application version 1.1"
+    return "flask application version 1.2"
 
 app.run(host="0.0.0.0", port=4000)
